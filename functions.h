90664e68697332a540c18f7e35d2615076822651
Ivona04 <ivonazivkovic04@gmail.com>
2024-09-19 01:56:27 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,6 +4,13 @@
 #define MAX_MODIFIKACIJA 3
 #define MAX_MODIFIKACIJA_TYP 21
 
+
+#define PERCENTAGE(x) ((x) * 100)
+
+static inline float izracunajPostotak(float osnovnaSnaga, float postotak) {
+    return osnovnaSnaga * (postotak / 100);
+}
+
 typedef enum {
     USPJEH,
     NEUSPJEH,
@@ -23,17 +30,20 @@ typedef struct {
     int brojModifikacija;
 } Motor;
 
-void unosPodatakaOMotoru(Motor* motor, int id);
+extern void unosPodatakaOMotoru(Motor* motor, int id);
 Status dodajMotor(Motor* motori, int* brojMotora);
 void prikaziMotore(const Motor* motori, int brojMotora);
 Status azurirajMotor(Motor* motori, int brojMotora, int id);
 Status obrisiMotor(Motor* motori, int* brojMotora, int id);
 float izracunajSnagu(const Motor motor);
 void ucitajModifikacijeIzDatoteke(const char* imeDatoteke);
-void prikaziModifikacije();
-void ucitajModifikacijeIzDatoteke(const char* imeDatoteke);
 void prikaziKategorijeModifikacija();
 void prikaziOpcijeModifikacija(const char* kategorija);
+void spremiMotoreUDatoteku(const char* imeDatoteke, Motor* motori, int brojMotora);
+int ucitajMotoreIzDatoteke(const char* imeDatoteke, Motor* motori, int* brojMotora);
 
+// Prototipovi za sortiranje i pretraživanje
+void sortirajMotore(Motor* motori, int brojMotora);
+Motor* pronadiMotor(Motor* motori, int brojMotora, int id);
 
-#endif //FUNCTIONS_H
+#endif // FUNCTIONS_H
