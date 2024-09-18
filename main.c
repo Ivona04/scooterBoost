2ed8dc3a763454193a43f507f703a0ef85356e7c
Ivona04 <ivonazivkovic04@gmail.com>
2024-09-19 01:56:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include "functions.h"
 
 #define MAX_MOTORA 10
@@ -12,50 +11,67 @@ static void prikaziIzbornik() {
     printf("2. Prikazi sve motore\n");
     printf("3. Azuriraj motor\n");
     printf("4. Obrisi motor\n");
-    printf("5. Izlaz\n");
+    printf("5. Sortiraj motore po osnovnoj snazi\n");
+    printf("6. Pretrazi motor po ID-u\n");
+    printf("7. Izlaz\n");
 }
 
 int main() {
-    Motor* motori = (Motor*) malloc(MAX_MOTORA * sizeof(Motor));
-
+    Motor* motori = (Motor*)malloc(MAX_MOTORA * sizeof(Motor));
     if (motori == NULL) {
         printf("Greska pri alociranju memorije.\n");
         exit(EXIT_FAILURE);
     }
 
-    ucitajModifikacijeIzDatoteke("./../modifikacije.txt");
-
     int brojMotora = 0;
     int opcija, id;
 
+    ucitajMotoreIzDatoteke("motori.txt", motori, &brojMotora);
+    ucitajModifikacijeIzDatoteke("modifikacije.txt");
+
     while (1) {
         prikaziIzbornik();
-        printf("Unesite opciju: ");
+        printf("\n\nUnesite opciju: ");
         scanf("%d", &opcija);
 
         switch (opcija) {
-            case 1:
-                dodajMotor(motori, &brojMotora);
+        case 1:
+            dodajMotor(motori, &brojMotora);
             break;
-            case 2:
-                prikaziMotore(motori, brojMotora);
+        case 2:
+            prikaziMotore(motori, brojMotora);
             break;
-            case 3:
-                printf("\nUnesite ID motora za azuriranje: ");
+        case 3:
+            printf("\nUnesite ID motora za azuriranje: ");
             scanf("%d", &id);
             azurirajMotor(motori, brojMotora, id);
             break;
-            case 4:
-                printf("\nUnesite ID motora za brisanje: ");
+        case 4:
+            printf("\nUnesite ID motora za brisanje: ");
             scanf("%d", &id);
             obrisiMotor(motori, &brojMotora, id);
             break;
-            case 5:
-                free(motori);
+        case 5:
+            sortirajMotore(motori, brojMotora);
+            printf("Motori su sortirani po osnovnoj snazi.\n");
+            break;
+        case 6:
+            printf("\nUnesite ID motora za pretragu: ");
+            scanf("%d", &id);
+            Motor* motor = pronadiMotor(motori, brojMotora, id);
+            if (motor) {
+                printf("Motor ID: %d, Osnovna snaga: %.2f\n", motor->motorId, motor->osnovnaSnaga);
+            }
+            else {
+                printf("Motor s ID: %d nije pronaden.\n", id);
+            }
+            break;
+        case 7:
+            free(motori);
             motori = NULL;
             return 0;
-            default:
-                printf("Nepoznata opcija. Pokusajte ponovo.\n");
+        default:
+            printf("Nepoznata opcija. Pokusajte ponovo.\n");
         }
     }
 
